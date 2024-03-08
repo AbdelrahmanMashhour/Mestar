725b991f0bb7c59ae1b04899bf637243e68eee5b
AbdelrahmanMashhour <abdofamous458@gmail.com>
2024-03-08 06:15:35 +0200

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPatternWithUOW.Core.Dto;
+using RepositoryPatternWithUOW.Core.DTOs;
+using RepositoryPatternWithUOW.Core.Interfaces;
+
+namespace Mestar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        IUnitOfWork unitOfWork;
+
+        public AccountsController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        [HttpPost("SignUp")]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var result = await unitOfWork.UserRepository.SignUpAsync(signUpDto);
+            if (!result)
+                return BadRequest();
+            await unitOfWork.SaveChangesAsync();
+
+            return Ok();
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var result = await unitOfWork.UserRepository.LoginAsync(loginDto);
+
+            if (result.Success)
+            {
+                await unitOfWork.SaveChangesAsync();
+                return Ok(result);
+            }
+            return NotFound();
+        }
+        [HttpPost("SendCode")]
+        public async Task<IActionResult> SendConfirmationCode(SendCodeDto sendCodeDto)
+        {
+            var result = await unitOfWork.UserRepository.SendVerficationCode(sendCodeDto.Email, sendCodeDto.Reset is null ? false : true);
+            if (!result)
+                return NotFound();
+            await unitOfWork.SaveChangesAsync();
+            return Ok();
+
+        }
+        [HttpPost("ValidateEmailVerificationCode")]
+
+        public async Task<IActionResult> ValidateConfirmationCode(ValidationCodeDto VCD)
+        {
+            var result = await unitOfWork.UserRepository.ValidateCode(VCD.Email, VCD.Code);
+            await unitOfWork.SaveChangesAsync();
+            if (!result)
+                return Forbid();
+            return Ok();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RepositoryPatternWithUOW.EF;
+using RepositoryPatternWithUOW.Core.OptionsPatternClasses;
+using RepositoryPatternWithUOW.EfCore.MailService;
+using System.Security.Claims;
+using System.Text;
+using RepositoryPatternWithUOW.EfCore.Mapper;
+using RepositoryPatternWithUOW.Core.Interfaces;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+var connstr = builder.Configuration.GetConnectionString("DefalutConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connstr).UseLazyLoadingProxies());
+
+builder.Services.Configure<TokenOptionsPattern>(builder.Configuration.GetSection("JWT"));
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddScoped<Mapper>();
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+builder.Services.AddTransient<IUnitOfWork,UnitOfWork>();
+builder.Services.AddTransient<ISenderService, MailService>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+var JwtSettings = builder.Configuration.GetSection("JWT").Get<TokenOptionsPattern>();
+builder.Services.AddSingleton(JwtSettings!);
+builder.Services.AddAuthentication(opts =>
+{
+    opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(opts =>
+    {
+
+        opts.RequireHttpsMetadata = true;
+        opts.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidAudience = JwtSettings.Audience,
+            ValidIssuer = JwtSettings.Issuer,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtSettings.SecretKey)),
+            RoleClaimType = ClaimTypes.Role,
+
+
+
+
+        };
+
+    });
+
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+        // Add JWT Bearer token authentication support
+        var securityScheme = new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Description = "JWT Authorization header using the Bearer scheme",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        };
+
+        c.AddSecurityDefinition("Bearer", securityScheme);
+
+        // Add JWT token authorization header
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
+    });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyHeader());
+app.MapControllers();
+
+app.Run();
